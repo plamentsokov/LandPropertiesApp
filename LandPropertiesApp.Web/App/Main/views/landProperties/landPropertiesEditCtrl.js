581b23a8ebc81a235fa80ca59f733681d861d5d5
Tsokov <plamentsokov@gmail.com>
2015-07-21 01:30:24 +0300

    Implement Add/Edit functionality with Angular js

--- LandPropertiesApp.Web/App/Main/views/landProperties/landPropertiesEditCtrl.js
+++ LandPropertiesApp.Web/App/Main/views/landProperties/landPropertiesEditCtrl.js
@@ -3,11 +3,21 @@
 
     angular
         .module('app')
-        .controller("LandPropertiesEditCtrl", ["landProperty", LandPropertiesEditCtrl]);
+        .controller("LandPropertiesEditCtrl", ["landProperty", "$state", LandPropertiesEditCtrl]);
 
-    function LandPropertiesEditCtrl(landProperty) {
+    function LandPropertiesEditCtrl(landProperty, $state) {
         var vm = this;
 
         vm.landProperty = landProperty;
+
+        vm.title = landProperty.Id ? "Edit land property N: " + landProperty.UPI : "New land property"
+
+        vm.submit = function (savedProduct) {
+            vm.landProperty.$save();
+            $state.go('landPropertiesList');
+        }
+        vm.cancel = function (savedProduct) {
+            $state.go('landPropertiesList');
+        }
     }
 })();
